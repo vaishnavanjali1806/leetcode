c02d8c1a82b0fa434d90e2e392fb34b51bd70732
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-02-23 12:19:13 +0530

    Update leet3.java

--- leet3.java
+++ leet3.java
@@ -1,1 +1,71 @@
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+         
+              int numOfNodes = preorder.length;
+        return constructTree(0, numOfNodes - 1, 0, preorder, postorder);
+    }
+
+    
+    private TreeNode constructTree(
+        int preStart,
+        int preEnd,
+        int postStart,
+        int[] preorder,
+        int[] postorder
+    ) {
+       
+        if (preStart > preEnd) return null;
+
+        
+        if (preStart == preEnd) {
+            return new TreeNode(preorder[preStart]);
+        }
+
+        int leftRoot = preorder[preStart + 1];
+
+       
+        int numOfNodesInLeft = 1;
+        while (postorder[postStart + numOfNodesInLeft - 1] != leftRoot) {
+            numOfNodesInLeft++;
+        }
+
+        TreeNode root = new TreeNode(preorder[preStart]);
+
+        
+        root.left = constructTree(
+            preStart + 1,
+            preStart + numOfNodesInLeft,
+            postStart,
+            preorder,
+            postorder
+        );
+
+       
+        root.right = constructTree(
+            preStart + numOfNodesInLeft + 1,
+            preEnd,
+            postStart + numOfNodesInLeft,
+            preorder,
+            postorder
+        );
+
+        return root;
+    }
+}
