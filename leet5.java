0b039e998e2f6aca32e5c5184de4377abf8b3351
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-02-25 15:44:16 +0530

    Update leet5.java

--- leet5.java
+++ leet5.java
@@ -1,1 +1,22 @@
+class Solution {
+    public int numOfSubarrays(int[] arr) {
+          final int MOD = (int) (1e9 + 7);
+        int n = arr.length;
+        int count = 0;
 
+        
+        for (int startIndex = 0; startIndex < n; startIndex++) {
+            int currentSum = 0;
+          
+            for (int endIndex = startIndex; endIndex < n; endIndex++) {
+                currentSum += arr[endIndex];
+              
+                if (currentSum % 2 != 0) {
+                    count++;
+                }
+            }
+        }
+
+        return count % MOD;
+    }
+}
