a6849180e8a0c81eb9b5c916071fd477bc6f4a7d
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-07-21 12:21:25 +0530

    Update leet36.java

--- leet36.java
+++ leet36.java
@@ -1,1 +1,32 @@
+public static int maxProfit(int[] prices) {
+    int min = prices[0];       
+    int profit = 0;            
 
+    for (int i = 1; i < prices.length; i++) {
+        if (prices[i] < min) {
+            min = prices[i];  
+        } else if (prices[i] - min > profit) {
+            profit = prices[i] - min;  
+        }
+    }
+
+    return profit;
+}public class Main {
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;  // handle empty array
+        }
+
+        int min = prices[0];       
+        int profit = 0;            
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];  
+            } else if (prices[i] - min > profit) {
+                profit = prices[i] - min;  
+            }
+        }
+
+        return profit;
+    }
