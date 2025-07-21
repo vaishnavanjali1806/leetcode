04b60c967c0ac45726c864c4aaffaf3d64b7976a
vaishnavanjali1806 <vaishnavanjali208@gmail.com>
2025-07-21 11:59:47 +0530

    Update leet35.java

--- leet35.java
+++ leet35.java
@@ -1,1 +1,22 @@
 
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (currentSum + nums[i] > nums[i]) {
+                currentSum = currentSum + nums[i];
+            } else {
+                currentSum = nums[i];
+            }
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+
+        return maxSum;
+    }
+};
